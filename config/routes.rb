6dff6a1f62dc512c39886e3433e8b6812928bad2
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-02-24 21:20:02 +0100

    Add messages from the ui

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+  post "messages", to: "messages#create"
   # Defines the root path route ("/")
   # root "articles#index"
 end
