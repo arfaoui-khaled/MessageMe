ec946395915e96b9635e307081d459a710f9b26b
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-02-25 02:00:57 +0100

    implement real time feature with action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: {maximum: 140}
-
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   post "messages", to: "messages#create"
+
+  mount ActionCable.server, at: '/cable'
   # Defines the root path route ("/")
   # root "articles#index"
 end
